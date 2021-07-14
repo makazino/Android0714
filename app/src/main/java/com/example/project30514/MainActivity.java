01395b1f2fcdd9579c6899ab1d5363394ddb4837
makazino <raphael1198@naver.com>
2021-07-14 10:34:04 +0900

    second one

--- app/src/main/java/com/example/project30514/MainActivity.java
+++ app/src/main/java/com/example/project30514/MainActivity.java
@@ -10,16 +10,17 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text1,text2;
-    CheckBox chkAgree;
+    Switch swAgree;
     RadioGroup rGroup1;
-    RadioButton rdodog,rdocat,rdorabbit;
-    Button btnOk;
+    RadioButton rdoOreo,rdoPie,rdoQ10;
+    Button btnQuit,btngoback;
     ImageView imgPet;
 
 
@@ -30,49 +31,57 @@ public class MainActivity extends AppCompatActivity {
         setTitle("애완동물 사진 보기");
 
         text1=findViewById(R.id.Text1);
-        chkAgree=findViewById(R.id.ChkAgree);
+        swAgree=findViewById(R.id.SwAgree);
         text2=findViewById(R.id.Text2);
         rGroup1=findViewById(R.id.Rgroup1);
-        rdodog=findViewById(R.id.Rdodog);
-        rdocat=findViewById(R.id.Rdocat);
-        rdorabbit=findViewById(R.id.Rdorabbit);
-        btnOk=findViewById(R.id.BtnOk);
+        rdoOreo=findViewById(R.id.RdoOreo);
+        rdoPie=findViewById(R.id.RdoPie);
+        rdoQ10=findViewById(R.id.RdoQ10);
+        btnQuit=findViewById(R.id.BtnQuit);
+        btngoback=findViewById(R.id.BtnGoBack);
         imgPet=findViewById(R.id.ImgPet);
-        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        swAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(chkAgree.isChecked()==true) {
+                if(swAgree.isChecked()==true) {
                     text2.setVisibility(View.VISIBLE);
                     rGroup1.setVisibility(View.VISIBLE);
-                    btnOk.setVisibility(View.VISIBLE);
                     imgPet.setVisibility(View.VISIBLE);
+                    btnQuit.setVisibility(View.VISIBLE);
+                    btngoback.setVisibility(View.VISIBLE);
                 }
                 else {
                     text2.setVisibility(View.INVISIBLE);
                     rGroup1.setVisibility(View.INVISIBLE);
-                    btnOk.setVisibility(View.INVISIBLE);
                     imgPet.setVisibility(View.INVISIBLE);
+                    btnQuit.setVisibility(View.INVISIBLE);
+                    btngoback.setVisibility(View.VISIBLE);
                 }
+                switch (rGroup1.getCheckedRadioButtonId()){
+                    case R.id.RdoOreo:
+                        imgPet.setImageResource(R.drawable.oreo);
+                        break;
+                    case R.id.RdoPie:
+                        imgPet.setImageResource(R.drawable.pie);
+                        break;
+                    case R.id.RdoQ10:
+                        imgPet.setImageResource(R.drawable.q10);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "버전을 먼저 선택하세요", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        btnQuit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (rGroup1.getCheckedRadioButtonId()){
-                    case R.id.Rdodog:
-                        imgPet.setImageResource(R.drawable.dog);
-                        break;
-                    case R.id.Rdocat:
-                        imgPet.setImageResource(R.drawable.cat);
-                        break;
-                    case R.id.Rdorabbit:
-                        imgPet.setImageResource(R.drawable.rabbit);
-                        break;
-                    default:
-                        Toast.makeText(MainActivity.this, "동물을 먼저 선택하세요", Toast.LENGTH_SHORT).show();
-                }
+                finish();
             }
         });
+
+
     }
 }
